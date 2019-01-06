5756e8134d21bef3efd1e2666449bacabe212f61
Md Sahil <287mdsahil@gmail.com>
2019-01-07 01:33:36 +0530

    Added button to insert to the books table
    button not yet fucntional

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,13 @@
+function bookDbInsert(){
+    //$('#bookDbInsert').css({"background-color":"rgb(80,80,80)"});
+    console.log("clicked");
+};
+
+
+
+//main function---------------------------------------------------------
 $(document).ready(function(){
-    $("#test").text("ready");
+    //var booksDb = openDatabase('booksDb','1.0','booksDb',1024*1024*1024);
+    console.log("success");
+    $('#bookDbInsert').click(bookDbInsert);
 })
